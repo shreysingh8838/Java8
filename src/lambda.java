0a7545a6162b7160ac97ddc446a9ffa3f02521ab
Shrey Singh <49588412+shreysingh8838@users.noreply.github.com>
2023-03-10 00:29:02 +0530

    1 Partial lambda added

--- src/lambda.java
+++ src/lambda.java
@@ -64,7 +64,7 @@ public class lambda {
 
         // For multiple lambda expression
         Shape square = () -> {
-            System.out.println("Print Square");
+            System.out.println("Print Square 2");
         };
         square.draw();
 
